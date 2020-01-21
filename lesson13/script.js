0f3999018633f055a81a37c60336d8a481be9775
asd <asd>
2020-01-21 19:35:45 +0200

    lesson 13

---
+++ lesson13/script.js
@@ -0,0 +1,47 @@
+// let a = 'value';
+// let b = a;
+
+// a = 'fixed_value';
+
+// console.log('a', a);
+// console.log('b', b);
+
+
+// const object = {
+//     name: '',
+//     hey: function(){
+//         console.log('Hey ' + this.name);
+//     }
+// }
+
+// object.name = 'Ivan';
+
+// object.hey();
+
+// class Cube{
+//     constructor(lenght, color){
+//         this.lenght = lenght;
+//         this.color = color;
+//     }
+
+//     getVolume = () => this.lenght ** 3;
+// }
+
+// const redCube = new Cube(5, "red");
+
+// console.log(redCube.getVolume());
+
+// BOM
+
+// location
+// history
+// screen 
+// fetch AJAX
+// navigator
+
+// const btn = document.querySelector('.AWESOME_BUTTON');
+
+// btn.addEventListener('click', function(event){
+//     console.log(event.x);
+    
+// });
